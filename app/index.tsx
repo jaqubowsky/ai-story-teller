6dd334f0de74fd7da49f095606c4be1ec8dd4d58
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-11-09 17:07:28 +0100

    chore: configure app, add supabase and google auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from 'react-native';
+import Auth from '@/components/Auth';
+import { StatusBar, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View className="">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Auth />
+      <StatusBar />
     </View>
   );
 }
